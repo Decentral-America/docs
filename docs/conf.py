ede9444410a4c147daa2fcd7c215f9e79cc8b5b5
jourlez <josuecr.288@gmail.com>
2021-11-02 19:49:02 -0600

    .

--- docs/conf.py
+++ docs/conf.py
@@ -41,8 +41,9 @@ release = '0.1'
 extensions = [
     'sphinx_copybutton',
     'sphinx_rtd_theme',
+    'sphinx_rtd_dark_mode',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages',	
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.mathjax',
 ]
@@ -84,7 +85,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx-rtd-dark-mode'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
